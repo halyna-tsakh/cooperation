c1937d3beb44f3ad989296c4cf5e42711832b33f
Onatolich <onatolich@gmail.com>
2017-07-19 12:47:44 +0200

    'done'

---
+++ assets/js/gallery.js
@@ -0,0 +1,51 @@
+$(function() {
+  var resizeTimer;
+  var portion = 0;
+  var $window = $(window);
+  var $tiles = $('#gallery .tile');
+  var $swiper = $('.swiper-container');
+
+  function displayPortion() {
+    portion = Number(!portion);
+    $tiles.find('.image');
+
+    $tiles.each(function(index) {
+      var $tile = $(this);
+
+      setTimeout(function() {
+        $tile.find('.image').addClass('hidden').eq(portion).removeClass('hidden');
+      }, index * 100);
+    });
+
+    setTimeout(displayPortion, 7000);
+  }
+
+  function updateImagesHeight() {
+    resizeTimer = undefined;
+
+    $tiles.each(function() {
+      var $tile = $(this);
+
+      $tile.height($tile.width());
+    });
+
+    $swiper.height($swiper.width());
+  }
+
+  $window.on('resize', function() {
+    if (!resizeTimer) {
+      resizeTimer = setTimeout(updateImagesHeight, 200);
+    }
+  });
+
+  new Swiper('.swiper-container', {
+    autoplay: 5000,
+    spaceBetween: 20,
+    loop: true,
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    simulateTouch: true
+  });
+  updateImagesHeight();
+  setTimeout(displayPortion, 7000);
+});
---
+++ assets/js/videoGallery.js
@@ -0,0 +1,29 @@
+$(function() {
+  var resizeTimer;
+  var aspectRatio = 218 / 388;
+  var $videos = $('#videoGallery .video');
+
+  function updateVideosHeight() {
+    resizeTimer = undefined;
+
+    $videos.each(function() {
+      var $video = $(this);
+      var $inner = $video.find('video');
+
+      $video.height($video.width() * aspectRatio);
+
+      $inner.attr({
+        width: $video.width() + 'px',
+        height: $video.height() + 'px'
+      });
+    });
+  }
+
+  $(window).on('resize', function() {
+    if (!resizeTimer) {
+      resizeTimer = setTimeout(updateVideosHeight, 200);
+    }
+  });
+
+  updateVideosHeight();
+});
