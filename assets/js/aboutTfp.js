ad7d66ea9445d8c6b1ed1736ac170623f6cd098b
Onatolich <onatolich@gmail.com>
2017-07-20 23:59:06 +0200

    upd

---
+++ assets/js/aboutTfp.js
@@ -0,0 +1,12 @@
+$(function() {
+  var $container = $('#aboutTfp');
+
+  $container.on('click', '.caption-container a', function(e) {
+    e.preventDefault();
+    $container.toggleClass('open');
+
+    if ($container.hasClass('open')) {
+      $('#layout').scrollTop(500);
+    }
+  });
+});
