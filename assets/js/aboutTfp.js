12efd326ce5a00d8a4a80ddb1c644935cfebeb7c
Onatolich <onatolich@gmail.com>
2017-07-21 10:41:31 +0200

    upd

--- assets/js/aboutTfp.js
+++ assets/js/aboutTfp.js
@@ -1,12 +1,18 @@
 $(function() {
+  var $layout = $('#layout');
+  var $wrap = $('#wrap');
   var $container = $('#aboutTfp');
+
+  function scrollToAboutTfp() {
+    $layout.animate({ scrollTop: $wrap.height() - $container.height() }, 500);
+  }
 
   $container.on('click', '.caption-container a', function(e) {
     e.preventDefault();
     $container.toggleClass('open');
 
     if ($container.hasClass('open')) {
-      $('#layout').scrollTop(2000);
+      setTimeout(scrollToAboutTfp, 0);
     }
   });
 });
